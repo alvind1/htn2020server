50cc3711b275cc888a6d553981897a740510d59f
alvind1 <alvin.dai.02@gmail.com>
2021-01-16 15:44:33 -0500

    yes

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ var pg = require('pg')
 const {config, connectString} = require('../config')
 /* GET users listing. */
 
-router.post('/', async(req, res) => {
+router.post('/image', async(req, res) => {
   var client = new pg.Client(config);
   console.log("HI");
 
@@ -18,7 +18,7 @@ router.post('/', async(req, res) => {
     return;
   });
 
-  output = fs.readFileSync('./Downloads/person1.jpg');
+  output = fs.readFileSync('./Downloads/donkey.jpg');
   encodedImage = new Promise(async (resolve, reject) => {
     try{
       resolve(await Buffer.from(output, 'binary').toString('base64'));
@@ -32,7 +32,7 @@ router.post('/', async(req, res) => {
     console.log("enc", encoded);
     
     command1 = `UPDATE users SET age = 11 WHERE firstname = 'userFirst'`;
-    command2 = `UPDATE users SET photos = '${encoded}' WHERE firstname = 'userFirst'`;
+    command2 = `UPDATE users SET photos = '${encoded}' WHERE username = 'olaf'`;
     client.query(command2)
     .then((output) => {
       console.log("good", output);
@@ -54,7 +54,7 @@ router.post('/', async(req, res) => {
   return;
 });
 
-router.get('/userFirst', async(req, res) => {
+router.get('/:username', async(req, res) => {
   try{
     var client = new pg.Client(config);
     console.log("HI");
@@ -65,11 +65,84 @@ router.get('/userFirst', async(req, res) => {
       res.send("uh oh");
       return;
     });
-  
-    client.query("SELECT * FROM users WHERE firstname = 'userFirst'")
+
+    client.query(`SELECT * FROM users WHERE username = '${req.params.username}'`)
     .then((output) => {
-      console.log("good", output);
-      res.send(output.rows[0].photos);
+      console.log("good");
+      res.send(output.rows[0]);
+    })
+    .catch((err) => {
+      throw err;
+    });
+
+  }catch(err){
+    console.log(err.message);
+    res.send("bad 19823");
+  }
+})
+
+router.delete("/:username", async(req, res) => {
+  try{
+    var client = new pg.Client(config);
+    console.log("HI");
+  
+    client.connect()
+    .catch((err) => {
+      console.log(err);
+      res.send("uh oh");
+      return;
+    });
+
+    client.query(`DELETE FROM users WHERE username = '${req.params.username}'`)
+    .then((output) => {
+      console.log("good");
+      res.send(output.rows[0]);
+    })
+    .catch((err) => {
+      throw err;
+    });
+
+  }catch(err){
+    console.log(err.message);
+    res.send("bad 19823");
+  }
+})
+
+router.put('/:username', async(req, res) => {
+  try{
+    var client = new pg.Client(config);
+    console.log("HI");
+  
+    client.connect()
+    .catch((err) => {
+      console.log(err);
+      res.send("uh oh");
+      return;
+    });
+
+    commandText = "";
+    if (req.body['firstName']){
+      commandText += `firstName = '${req.body['firstName']}',`
+    }
+
+    if(req.body['lastName']){
+      commandText += `lastName = '${req.body['lastName']}',`
+    }
+
+    if(req.body['age']){
+      commandText += `age = ${req.body['age']},`
+    }
+
+    if(req.body['email']){
+      commandText += `email = '${req.body['email']}',`
+    }
+
+    commandText = commandText.substring(0, commandText.length-1);
+
+    client.query(`UPDATE users SET ` + commandText + ` WHERE username = '${req.params.username}'`)
+    .then((output) => {
+      console.log("good12");
+      res.send(output.rows[0]);
     })
     .catch((err) => {
       throw err;
@@ -99,14 +172,19 @@ router.put('/', async(req, res) => {
 
     //output = await client.query("ALTER TABLE users DROP COLUMN photos");
 
-    output = await client.query("ALTER TABLE users ADD COLUMN photos text");
+    //output = await client.query("ALTER TABLE users ADD COLUMN photos text");
+
+    //output = await client.query("ALTER TABLE users ADD COLUMN userId ");
+
+    output = await client.query("UPDATE users SET username = 'testUsername' WHERE firstName = 'userFirst'");
 
     res.send("good123768");
   }catch(err){
     console.log(err.message, "bad123187");
     res.send("bad1824");
   }
-})
+});
+
 router.get('/woot', async (req, res, next) => {
     var client = new pg.Client(config);
     client.connect();
@@ -116,7 +194,7 @@ router.get('/woot', async (req, res, next) => {
 });
 
 router.post('/register', async (req, res, next) => {
-    let input = req.body;
+    let input = req.body; //necessary input fields are firstname, lastname, email, password, username, phone
     var client = new pg.Client(config);
     await client.connect();
 
@@ -150,12 +228,8 @@ router.post('/login', async (req, res, next) => {
     res.send(user.rows[0]);
 });
 
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res, next) => { //gets all info about user
   try{
-    //console.log(connectString);
-    //var pool = new pg.Pool(connectString);
-    //console.log(config);
-
     var client = new pg.Client(config);
     console.log("HI");
 
@@ -166,17 +240,11 @@ router.get('/', async (req, res, next) => {
       return;
     });
 
-    returnVal = 'respond with a resource';
-    let output = 1;
-
-    output = fs.readFileSync('./Downloads/person1.jpg');
-    
-    //encodedImage = await Buffer.from(output, 'binary').toString('base64');
-
-
-    returnVal = encodedImage;
-    console.log("output = ", encodedImage);
-    res.send(encodedImage);
+    client.query("SELECT * FROM users")
+    .then((users) => {
+      console.log("got users");
+      res.send(users);
+    })
   }catch(err){
     console.log("!!!", err);
     res.send("yikes")
